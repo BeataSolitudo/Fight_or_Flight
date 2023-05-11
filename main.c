425d7fda23a2484ffa88579d8f09c7a4f0c3a0b5
BeataSolitudo <j.kalivoda.st@spseiostrava.cz>
2023-05-11 23:42:31 +0200

    V0.2

--- main.c
+++ main.c
@@ -3,87 +3,144 @@
 #include <string.h>
 #include <time.h>
 #include <conio.h>
+#include <stdbool.h>
 
-#define VEL 10
-
-typedef struct{
-    int HP;
-    float LVL;
-    int power;
-    char *specialni_schopnost[VEL];
-} POSTAVA;
-
-void moznosti_pro_vyfuka_1()
-{
-    printf("Tve dny jsou secteny, ponevadz na tebe doma ceka matka, neni uniku.\n");
-    printf("GAME OVER\n");
-}
-
-void moznosti_pro_vyfuka_2()
-{
-    printf("Jsi student, nemas penize.\n");
-    printf("GAME OVER\n");
-}
+void janitor(float *player_HP, float *player_disengage_LVL, float *player_power, float *player_overall_LVL, float *player_cash, bool *janitor_B);
 
 int main() {
     // Variables
-        //NPCs
-        char moznosti_pro_vyfuka;
-        char moznosti_pro_recepcni;
+    //Game
+    int current_stage = 1;  /* 1)Janitor 2) */
 
-        //Player
-        int player_HP;
-        int player_disengage_LVL;
-        int player_power;
-        float player_overall_LVL;
+    //NPCs
+    bool janitor_B = 0;
+
+    //Player
+    float player_HP = 100;
+    float player_disengage_LVL = 10;
+    float player_power = 10;
+    float player_overall_LVL;
+    float player_cash = 50;
+
+    player_overall_LVL = player_HP+player_disengage_LVL+player_power;
+
+    // Initial dialog
+    printf("You are just entering the school, as always late, but this time you have forgotten your ISIC, you ask if the janitor will open the door for you, he doesn't, now you are being challenged by the janitor, ISIC or life he tells you.\n");
+
+    // Main-loop
+    do{
+
+        switch(current_stage)
+        {
+            case 1:
+                if(janitor_B == 1){continue;}
+                janitor(&player_HP, &player_disengage_LVL, &player_power, &player_overall_LVL, &player_cash, &janitor_B);
+                break;
 
 
-    //Vyfuk
-    printf("Prave vstupujes do skoly, jako vzdy pozde, avsak tentokrat sis zapomnel ISIC, optas se, zda ti Vyfuk otevre, neotevira, nyni jsi vyzvan Vyfukem, ISIC nebo zivot povida.\n");
-    printf("Tve moznosti jsou: 1) Jit Domu. 2) Uplatit. 3)Vytahnout glock. 4) Jit skrze hlavni vchod\n");
-    moznosti_pro_vyfuka = getch();
-    system("cls");
+        }
 
-    switch(moznosti_pro_vyfuka){
-        case '1':
-            moznosti_pro_vyfuka_1();
-            return 1;
 
-        case '2':
-            moznosti_pro_vyfuka_2();
-            return 1;
 
-        case '3':
-            printf("test\n");
-            break;
 
-        case '4':
-            printf("Prosel jsi, nyni vstupujes do skoly, recepcni ti nabizi kridu, prijmes tuto pochutinu?\n");
-            printf("1) Ano. 2) Ne.\n");
-            scanf("%s", &moznosti_pro_recepcni);
-            break;
 
-        default: printf("ERROR");
+
+
     }
+    while(1==1);
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    return 0;
 }
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void janitor(float *player_HP, float *player_disengage_LVL, float *player_power, float *player_overall_LVL, float *player_cash, bool *janitor_B)
+{
+float player_HP_f = *player_HP;
+float player_disengage_LVL_f = *player_disengage_LVL;
+float player_power_f = *player_power;
+float player_overall_LVL_f = *player_overall_LVL;
+float player_cash_f = *player_cash;
+bool janitor_B_f = *janitor_B;
+
+
+char janitor_options;
+float janitor_income;
+float janitor_power_LVL;
+
+    printf("You have met the janitor, your options are: \n");
+    printf("1) Go home. 2) Bribe. 3) Fight.\n");
+    printf("\n");
+    printf("Your HP: %f\n", player_HP_f);
+    printf("Your disengage LVL: %f\n", player_disengage_LVL_f);
+    printf("Your Power: %f\n", player_power_f);
+    printf("You have %f cash\n", player_cash_f);
+    janitor_options = getch();
+
+    srand(time(NULL));
+    janitor_income = rand() % (600 - 400 + 1) + 400;
+    janitor_power_LVL = rand() % (50 - 15 + 1) + 15;
+
+    switch(janitor_options)
+    {
+        case '1':
+            printf("Your days are numbered, since your mother is waiting for you at home, there is no escape.\n");
+            printf("Game Over");
+            exit(0);
+
+        case '2':
+            if(player_cash_f >= janitor_income){
+                printf("The janitor says, I'll let you pass this time. %f Kc\n", janitor_income);
+                player_cash_f = player_cash_f - janitor_income;
+                printf("Your cash is %f Kc.\n", player_cash_f);
+            }
+            else{
+                printf("You're a student, you have no money.\n");
+                exit(0);
+            }
+
+        case '3':
+            player_HP_f = player_power_f - janitor_power_LVL;
+            printf("Bring it on butter ball.\n");
+            if(player_power_f <= janitor_power_LVL && player_HP_f > 0){
+                printf("You are fucked, there is no escape.\n");
+                printf("Game Over\n");
+                exit(0);
+            }
+            else{
+                printf("The fight was fierce, but you won, you can pass. Your power has been increased and HP decreased\n");
+                player_power_f += 25;
+                printf("Your power is: %f\n", player_power_f);
+                printf("Your HP: %f\n", player_HP_f);
+                janitor_B_f = 1;
+            }
+            break;
+
+    }
+
+// Here you return.
+    *player_HP = player_HP_f;
+    *player_disengage_LVL = player_disengage_LVL_f;
+    *player_power = player_power_f;
+    *player_overall_LVL = player_overall_LVL_f;
+    *player_cash = player_cash_f;
+    *janitor_B = janitor_B_f;
+
+}
